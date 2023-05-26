0cfabbe7a1735b5fabcacfdc9f65f091ea19ba46
vinicius <vinicius.dogonski@gmail.com>
2023-05-25 22:18:46 -0300

    banner

---
+++ src/section/Banner/index.js
@@ -0,0 +1,15 @@
+import './index.css';
+
+function Banner() {
+    return (
+        <div class="centralizar">
+            <div class="central">
+                <div className="central">
+                <h1>CORES</h1>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Banner;
